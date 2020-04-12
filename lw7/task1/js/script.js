8da748f37ebc8b4db108ed4ad3c531187c5e3c6f
kael-pam-pam <59134677+kael-pam-pam@users.noreply.github.com>
2020-04-12 14:44:52 +0300

    Правки в ЛР №7

--- lw7/task1/js/script.js
+++ lw7/task1/js/script.js
@@ -6,25 +6,26 @@ function isPrimeNumber(n) {
     return;
   }
   
-  if (typeof(n) == 'number') {
-    if (n < 2) {
+  if (!Number.isInteger(n)) {
+    console.log('Ошибка. Переданный параметр ', n, 
+                ' не является целым числом либо массивом, либо элемент массива не целое число');
+    return;
+  }  
+  
+  if (n < 2) {
+    isPrime = false;
+  }
+
+  for (let i = 2; isPrime && i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
       isPrime = false;
     }
-    
-    for (let i = 2; isPrime && i <= Math.sqrt(n); i++) {
-      if (n % i == 0) {
-        isPrime = false;
-      }
-    }
-    
-    if (isPrime) {
-      console.log(n + ' is prime number');
-    } else {    
-      console.log(n + ' is not prime number');
-    }
-    return;
+  }
+
+  if (isPrime) {
+    console.log('', n, ' is prime number');
+  } else {    
+    console.log('', n, ' is not prime number');
   } 
   
-  console.log('Ошибка. Переданный параметр "' + n + 
-              '" не является числом либо массивом, либо элемент массива не число');
 }
